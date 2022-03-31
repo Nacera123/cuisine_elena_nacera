5c00ef00e46ea7c9a0c46ba1966d7812859347fe
Nacera123 <nace.slimane@gmail.com>
2022-03-31 15:22:35 +0200

    Interface et 2eme page

--- src/Interface.java
+++ src/Interface.java
@@ -1,80 +1,211 @@
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 
-
-
-import javafx.stage.*;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 import javafx.scene.*;
 import javafx.scene.canvas.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.*;
 import javafx.scene.image.*;
 
-
-
-
+import java.awt.*;
 
 
 public class Interface extends Application {
+    /**
+     * le stage est notre theatre c'est la premiere chose qu'on crée
+     * on peut lui donner un titre
+     * tout ce qui est mis a l'interieur du stage est une scene "la scene est une partie de mon application"
+     * pour créer la scene, il faut créer un objet de type borderpane et lui donner un nom ici on va l'appeler root pour dire que c'est une racine c'est lui qui va contenir tous les element que je vais afficher
+     * juste apres je crée ma scéne, et lui donner pour parametre le nom de mon borderpane
+     * exp: Scene scene= new Scene(root); si on veut donner des dimensions a ma scene on met des virgules apres le root et on dimentionne
+     * pour que la scene soit prise en compte faut la mettre a l'interieur du stage
+     * pour cela on fait : stage.setScene(scene);
+     */
 
 
-    @Override
-    public void start(Stage stage) throws Exception {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+        @Override
+        public void start(Stage stage) throws Exception {
 
 
-        Group root = new Group();
-        Scene theScene = new Scene(root);
-        stage.setScene(theScene);
-        Canvas canvas = new Canvas( 500, 500 );
-        root.getChildren().add( canvas );
-        GraphicsContext gc =canvas.getGraphicsContext2D();
-        gc.setFill( Color.BLUE );
-        gc.setStroke( Color.BLACK );
-        gc.setLineWidth(2);
-        Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
-                gc.setFont( theFont );
-        gc.fillText( "Hello, World!", 40, 50 );
-        gc.strokeText( "Hello, World!", 40, 50 );
-        Image earth = new Image( "https://nimax-img.de/Produktbilder/zoom/59661_1/Globe-a-relief-AstroReality-EARTH.jpg" );
-        gc.drawImage( earth, 40, 50 );
-        stage.show();
+            // le titre du site
+            Label titre= new Label("La Vraie Recette");
 
-        final long startNanoTime = System.nanoTime();
-        new AnimationTimer()
-        {
-            public void handle(long currentNanoTime)
-            {
-                double t = (currentNanoTime -
-                        startNanoTime) / 1000000000.0;
-                double x = 232 + 128 * Math.cos(t);
-                double y = 232 + 128 * Math.sin(t);
-//                gc.drawImage( space, 0, 0 );
-//                gc.drawImage( earth, x, y );
-//                gc.drawImage( sun, 196, 196 );
-            }
-        }.start();
+            //le font du titre du site
+            Font fontTitre = Font.font("Times New Roman", FontWeight.BOLD, 40);
+            titre.setFont(fontTitre);
+
+            titre.setTextFill(Color.rgb(176,224,230));
+            titre.setTranslateX(400);
+            titre.setTranslateY(20);
+
+            // text menu du jour
+            Label labelMenuDuJour = new Label("Menu Du Jour");
+            labelMenuDuJour.setTranslateY(250);
+            labelMenuDuJour.setTranslateX(500);
+
+            //font size pour le menu du jour
+            Font fontMenu = Font.font("Times New Roman", FontWeight.BOLD, 30);
+            labelMenuDuJour.setFont(fontMenu);
+
+
+
+            //la barre de recherche
+            TextField textFieldnNomPlat = new TextField();
+            textFieldnNomPlat.setTranslateX(50);
+            textFieldnNomPlat.setTranslateY(30);
+
+            // Bouton et son style
+            Button rechercher= new Button("Recherche");
+            rechercher.setTranslateY(30);
+            rechercher.setTranslateX(20);
+            rechercher.setStyle("-fx-background-color: #87CEEB; ");
+
+            // rendre mon bouton opperationnel
+            rechercher.setOnAction(value ->  {
+                rechercher.setText("Clicked!");
+            });
+
+            //Bouton affiner ma recherche
+            Button buttonAffinnerRecherche = new Button("Affiner ma recherche");
+            buttonAffinnerRecherche.setTranslateY(50);
+            buttonAffinnerRecherche.setTranslateX(100);
+            buttonAffinnerRecherche.setStyle("-fx-background-color: #87CEEB;");
+
+            //la hbox qui va contenir la barre de recherche + affiner ma recherche
+            VBox vboxTextField = new VBox();
+            vboxTextField.setPrefSize(250,110);
+            vboxTextField.getChildren().addAll(textFieldnNomPlat,buttonAffinnerRecherche);
+
+            //la hbox qui va contenir le bouton rechercher
+            HBox hboxBouton = new HBox();
+            hboxBouton.setPrefSize(100,110);
+            hboxBouton.getChildren().add(rechercher);
+
+
+            // creer une Vbox qui va contenir a droite une Hbox  avec le bouton rechercher et a gauche le TextField
+            HBox hboxrecherche = new HBox();
+            hboxrecherche.setPrefSize(400,110);
+            hboxrecherche.setTranslateY(100);
+            hboxrecherche.setTranslateX(100);
+            hboxrecherche.setSpacing(40);
+
+            hboxrecherche.getChildren().addAll(vboxTextField,hboxBouton);
+
+
+
+
+
+            //pour l'image en arriere plan
+            Image background = new Image( "photo.png");
+            ImageView backgroundView = new ImageView(background);
+
+            // pour l'image du petit dejeuner
+            Image imagePetitDejeuner = new Image("petitdejeuner.png");
+            ImageView backgroundPetitDejeuner = new ImageView(imagePetitDejeuner);
+            //backgroundPetitDejeuner.layoutXProperty(w);
+
+            // le font pour les intitules des checkbox
+            Font intituleCheckBox= Font.font( "Times New Roman", FontWeight.BOLD, 15);
+
+            // le titre pour la partie regime alimentaire
+            Label labelPetitDejeuner= new Label("Petit Dejeuner");
+            labelPetitDejeuner.setFont(intituleCheckBox);
+
+
+            // le titre pour la partie type de plat(petit dejeuner, diner, dejeuner)
+            Label labelDejeuner= new Label("Dejeuner");
+            labelDejeuner.setFont(intituleCheckBox);
+
+            // le titre pour la partie Ingredient
+            Label labelDiner = new Label("Diner");
+            labelDiner.setFont(intituleCheckBox);
+
+            // les Vbox qui vont contenir les checkbox
+            //Vbox1
+            VBox lesCheck =new VBox();
+
+            lesCheck.setPrefSize(200,50);
+            //Vbox2
+            VBox lesCheck1 = new VBox();
+
+            lesCheck1.setPrefSize(200,50);
+            //Vbox2
+            VBox lesCheck2 =new VBox();
+
+            lesCheck2.setPrefSize(200,50);
+
+
+
+            // Inserer les titre et les chckbox dans chacune des Vbox
+            //Insertion pour la Vbox1
+            lesCheck.getChildren().add(backgroundPetitDejeuner);
+            lesCheck.setSpacing(30); //espace entre tous les element
+
+            //Insertion pour la Vbox2
+            lesCheck1.getChildren().addAll(labelDejeuner);
+            lesCheck1.setSpacing(30);
+
+            //Insertion pour la Vbox3
+            lesCheck2.getChildren().addAll(labelDiner);
+            lesCheck2.setSpacing(30);
+
+
+
+            // la hbox qui contient toutes les Vbox et son style
+            HBox divConteneur=new HBox();
+            divConteneur.getChildren().addAll(lesCheck, lesCheck1, lesCheck2);
+            divConteneur.setSpacing(100);
+            divConteneur.setTranslateY(100);
+
+            divConteneur.setLayoutX(200);
+            divConteneur.setLayoutY(200);
+            divConteneur.setPrefSize(800,300);
+            divConteneur.setStyle("-fx-background-color:#DCDCDC;");
+
+            // Mon root
+            Group root1 =new Group();
+            root1.setLayoutX(0);
+            root1.setLayoutY(0);
+
+
+            root1.getChildren().addAll(backgroundView,titre,  hboxrecherche,divConteneur,labelMenuDuJour);
+
+
+            //La scene
+            Scene checkScene = new Scene(root1);
+
+            // le stage et ses dimensions
+            stage.setScene(checkScene);
+            stage.setWidth(1100);
+            stage.setHeight(650);
+            stage.setTitle("checkbox");
+            stage.show();
+
+
+
+
+
+        }
     }
 
 
-}
-//    public static void main(String[] args) {
-//
-//    }
-
-//    Text text = new Text
-//pour importer javafx:
-//1-aller dans file
-//project structure-->librairie--> plus-> chercher le lib de javafx:-> l'ajouter
-//3-run->edit configuration->modify option->add VM option-> copier coller ce message:--module-path ${PATH_TO_FX} --add-modules javafx.controls,javafx.fxml,javafx.base,javafx.graphics,javafx.swing,javafx.media
-//->apply ->ok
 
 
-//    Scene scene = new scene(root,height,width);
-//
-//    scene.setFill(Color.WHITE);
-//    stage.setTitle("test")
-//
-//   //add text
-//    Text text = new Text();
-//    text.setFont(new Font(v 45));
-//    text.setsiza
+
+
+
--- src/LaVraieRecette/Interface.java
+++ src/LaVraieRecette/Interface.java
@@ -1,10 +1,14 @@
+//
 package LaVraieRecette;
+
+
 
 public class Interface {
     public static void main(String[] args) {
 
-    }
 
+
+    }
 
 
 
