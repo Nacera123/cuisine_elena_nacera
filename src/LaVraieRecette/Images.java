00029d65f4a0902092daf97a0f3618920f83c2e3
Elena <elenaelenoglou15@gmail.com>
2022-04-01 11:34:06 +0200

    Elena 01.04

---
+++ src/LaVraieRecette/Images.java
@@ -0,0 +1,32 @@
+package LaVraieRecette;
+
+import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+
+
+public class Images extends JFrame {
+
+    JPanel jp = new JPanel();
+    JLabel jl = new JLabel();
+
+    public Images(){
+        setTitle("Tiramisu");
+        setVisible(true);
+        setSize(800,400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        jl.setIcon(new ImageIcon("C:\\Users\\elena\\Documents\\POEI\\cuisine_elena_nacera\\cuisine_elena_nacera\\src\\images\\tiramisu2.png"));
+        jp.add(jl);
+
+        add(jp);// add janel to frame
+
+        validate(); // validate image when start running it
+    }
+
+    public static void main(String[] args) {
+        Images im = new Images();
+    }
+}
